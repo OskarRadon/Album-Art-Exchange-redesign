16d72778c3e61d2e3e565dd48017eb6ee4969b72
Oskar Radon <oskarradonkimball@gmail.com>
2016-05-09 14:04:34 -0700

    SetInterval not SetTimeout!!!

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,26 @@
-//
-// var random = getRandomInt();
-// console.log(random);
-//
-// window.onload = function() {
-//
-//   function generateImage() {
-//     for (i = 1; i <= 9; i++) {
-//       document.getElementById("a" + i).style.backgroundImage = "url('img/" + random + ".jpg')";
-//     }
-//
-//   }
-//   setTimeout(generateImage, 1000);
-//
-// }
-
-var rotate = false;
-function setbackground() {
-  window.setTimeout( setbackground, 1000);
-  var newImage = rotate ? 'url(img/' + 1 +'.jpg)' : 'url(img/' + 2 + '.jpg)';
-  rotate = !rotate;
-  document.getElementById('a1').style.backgroundImage = newImage;
-}
-
 function getRandomInt() {
- return Math.floor(Math.random() * (17) + 1);
+ var random = Math.floor(Math.random() * (17) + 1);
+ console.log(random);
+ return random;
 }
+
+window.onload = function() {
+  var random;
+  function generateImage() {
+    random = getRandomInt();
+    for (i = 1; i <= 9; i++) {
+      document.getElementById("a" + i).style.backgroundImage = "url('img/" + random + ".jpg')";
+    }
+
+  }
+  setInterval(generateImage, 1000);
+
+}
+
+// var rotate = false;
+// function setbackground() {
+//   window.setTimeout( setbackground, 1000);
+//   var newImage = rotate ? 'url(img/' + 1 +'.jpg)' : 'url(img/' + 2 + '.jpg)';
+//   rotate = !rotate;
+//   document.getElementById('a1').style.backgroundImage = newImage;
+// }
