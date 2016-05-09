1eae7a7f30805186e9cd6aa3f374ad0d59c82fe5
Oskar Radon <oskarradonkimball@gmail.com>
2016-05-09 10:49:20 -0700

    Add function to set background image of each div
    Doesn’t work yet - can’t target div using class name 😓

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,16 @@ window.onload = function() {
   function getRandomInt() {
     return Math.floor(Math.random() * (18) + 1);
   }
+
+  var random = getRandomInt();
+  console.log(random);
+
+  function generateImage() {
+    for (i = 1; i <= 9; i++) {
+      document.getElementByClassName("album").style.backgroundImage = "url('1.jpg')";
+    }
+
+  }
+  setTimeout(generateImage, 1000);
+
 }
