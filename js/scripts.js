070428c816075ea4335861b4b057f4c9d767ea83
Oskar Radon <oskarradonkimball@gmail.com>
2016-05-09 14:08:49 -0700

    Refactor and clean up js

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,16 @@
 function getRandomInt() {
- var random = Math.floor(Math.random() * (17) + 1);
- console.log(random);
- return random;
+  return Math.floor(Math.random() * (17) + 1);
 }
 
 window.onload = function() {
-  var random;
+
   function generateImage() {
     random = getRandomInt();
     for (i = 1; i <= 9; i++) {
       document.getElementById("a" + i).style.backgroundImage = "url('img/" + random + ".jpg')";
     }
-
   }
-  setInterval(generateImage, 1000);
+  setInterval(generateImage, 3000);
 
 }
 
